8409e1c3ad531b00229316a90363899c3aebf835
Quentin-JOSEPH <quentin.joseph.jq@gmail.com>
2024-04-30 09:47:17 +0200

    api

--- src/app/common/services/api.service.ts
+++ src/app/common/services/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,44 @@ import { Observable } from 'rxjs';
 export class ApiService {
   private apiUrl = 'https://www.adresse.com/mv';
 
-  constructor(private http: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
   // Function to perform GET request
   getData(endpoint: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/${endpoint}`);
+    return this.httpClient.get(`${this.apiUrl}/${endpoint}`);
   }
 
-  getCredential() {
-    this.getData('')
+  private load<T>(URL: string, module: string, data: any): Observable<T> {
+    let _headers = new HttpHeaders({
+      'LocalAPI': `true`
+    });
+    // if (behavior === APIBehavior.SkipReviver) {
+    //     if (localStorage.Token !== undefined && environment.useJWT) {
+    //         _headers.append('SkipReviver', `true`);
+    //     }
+    // }
+    return this.httpClient.post<T>(`${environment.API_URL}${module}/${URL}`, data, { headers: _headers }).pipe(catchError(this.handleError));
+  }
 
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error API: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(() => new Error(errorMessage));
+  }
+
+  getCredential(credential: Credential) {
+    //TODO check who  is better (I guess load)
+    this.getData('');
+
+    return this.load('getCredential', 'connect', credential);
+
+    //exemple for get
+    //return this.load(`GetQueueListItem?accountNumber=${accountNumber}`, 'Prospection', null)
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: "http://localhost:5000/api/",// TODO : change
+  appVersion: require('../../package.json').version,
 };
